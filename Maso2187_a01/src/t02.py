de22779f3f0bc870f0136179a313e114386884fc
ClearAspect <roanmason@live.ca>
2023-09-25 14:03:05 -0400

    Assignment 1

---
+++ Maso2187_a01/src/t02.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-09-20"
+-------------------------------------------------------
+"""
+
+
+# Imports
+
+# Constants
+
+age = int(input("What is your age? "))
+band = input("What is your favourite band? ")
+
+print("I am",age,"years old and",band,"is my favourite band.")
---
+++ Maso2187_a01/src/t03.py
@@ -0,0 +1,21 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-09-20"
+-------------------------------------------------------
+"""
+
+
+# Imports
+
+# Constants
+M_TO_KM = 1.61
+
+length_miles = float(input("Length in miles: "))
+length_kilometers = length_miles * M_TO_KM
+
+print("Length in km:",length_kilometers)
