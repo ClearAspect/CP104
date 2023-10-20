b9984b0e934199c25585da781d7f02e8ee3a9c5f
ClearAspect <roanmason@live.ca>
2023-10-20 10:27:27 -0400

    Stuff

---
+++ Maso2187_l05/src/functions.py
@@ -0,0 +1,43 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-10-17"
+-------------------------------------------------------
+"""
+
+
+# Imports
+
+# Constants
+ACC_GRAVITY = 9.8
+
+def get_weight(mass):
+    """
+    -------------------------------------------------------
+    Describes a mass in terms of its weight. If its weight is > 1000 N,
+    it is "heavy", less than 10 N it is "light", and "average" otherwise.
+    weight = mass (kg)  acceleration due to gravity (9.8/m/s^2)
+    Use: weight, message = get_weight(mass)
+    -------------------------------------------------------
+    Parameters:
+        mass - mass of an object in kg (float > 0)
+    Returns:
+        weight - weight of an object in Newtons (float)
+        message - description of weight of object (str)
+    -------------------------------------------------------
+    """
+    weight = mass * ACC_GRAVITY
+    if weight > 1000:
+        message = "heavy"
+    elif weight < 10:
+        message = "light"
+    else:
+        message = "average"
+
+    return weight, message
+
+
---
+++ Maso2187_l05/src/t02.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-10-17"
+-------------------------------------------------------
+"""
+# Imports
+from functions import get_weight
+
+# Constants
+
+weight, averge = get_weight(1000)
+print(weight, averge)
