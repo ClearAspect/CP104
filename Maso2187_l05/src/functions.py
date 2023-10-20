0babdbe1b985adb24e9da66ee53ec30e8b441f30
ClearAspect <roanmason@live.ca>
2023-10-20 19:15:05 -0400

    Changes

--- Maso2187_l05/src/functions.py
+++ Maso2187_l05/src/functions.py
@@ -41,3 +41,94 @@ def get_weight(mass):
     return weight, message
 
 
+def is_leap(year) -> bool:
+    """
+    -------------------------------------------------------
+    Determines if a year is a leap year. Every year that is
+    exactly divisible by four is a leap year, except for years
+    that are exactly divisible by 100, but these centurial years
+    are leap years if they are exactly divisible by 400. For
+    example, the years 1700, 1800, and 1900 are not leap years,
+    but the years 1600 and 2000 are.
+    Use: result = is_leap(year)
+    -------------------------------------------------------
+    Parameters:
+        year - a year (int > 0)
+    Returns:
+        result - True if year is a leap year,
+            False otherwise (boolean)
+    ------------------------------------------------------
+    """
+    result = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    return result
+
+def roman_numeral(n):
+    """
+    -------------------------------------------------------
+    Convert 1-10 to Roman numerals.
+    Use: numeral = roman_numeral(n)
+    -------------------------------------------------------
+    Parameters:
+        n - number to convert to Roman numerals (int)
+    Returns:
+        numeral - Roman numeral version of n, None if n is not
+          between 1 and 10 inclusive. (str)
+    -------------------------------------------------------
+    """
+    if n == 1:
+        numeral = "I"
+    elif n == 2:
+        numeral = "II"
+    elif n == 3:
+        numeral = "III"
+    elif n == 4:
+        numeral = "IV"
+    elif n == 5:
+        numeral = "V"
+    elif n == 6:
+        numeral = "VI"
+    elif n == 7:
+        numeral = "VII"
+    elif n == 8:
+        numeral = "VIII"
+    elif n == 9:
+        numeral = "IX"
+    elif n == 10:
+        numeral = "X"
+    else:
+        numeral = None
+
+    return numeral
+
+def quadrant(x, y):
+    """
+    -------------------------------------------------------
+    Determines location on a plane of an x, y coordinate.
+    Use: location = quadrant(x, y)
+    -------------------------------------------------------
+    Parameters:
+        x - x coordinate on a Cartesian plane (float)
+        y - y coordinate on a Cartesian plane (float)
+    Returns:
+        location - one of: 'Quadrant 1', 'Quadrant 2', 'Quadrant 3'
+          'Quadrant 4', 'X-Axis', 'Y-Axis', 'Origin' (str)
+    -------------------------------------------------------
+    """
+    if x == 0 and y == 0:
+        location = "Origin"
+    elif x == 0:
+        location = "Y-Axis"
+    elif y == 0:
+        location = "X-Axis"
+    elif x > 0 and y > 0:
+        location = "Quadrant 1"
+    elif x < 0 and y > 0:
+        location = "Quadrant 2"
+    elif x < 0 and y < 0:
+        location = "Quadrant 3"
+    elif x > 0 and y < 0:
+        location = "Quadrant 4"
+    else:
+        location = None
+
+    return location
