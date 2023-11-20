c3aa084d107010c96baaf63ea2073cc2e076e365
ClearAspect <roanmason@live.ca>
2023-11-20 14:50:13 -0500

    lab 10

---
+++ Maso2187_l10/src/functions.py
@@ -0,0 +1,46 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-11-20"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use:
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+def customer_record(fh, n):
+    """
+    -------------------------------------------------------
+    Find the n-th record in a comma-delimited sequential file.
+    Records are numbered starting with 0.
+    Use: result = customer_record(fh, n)
+    -------------------------------------------------------
+    Parameters:
+        fh - file to search (file handle - already open for reading)
+        n - the number of the record to return (int > 0)
+    Returns:
+        result - a list of the fields of the n-th record if it exists,
+            an empty list otherwise (list)
+    -------------------------------------------------------
+    """
+
+    customer_record = open("customers.txt", "r", encoding="utf-8")
