2e62807b27504ff5cf74f7ae4505394da2f2aaba
ClearAspect <roanmason@live.ca>
2023-11-30 19:29:21 -0500

    Lab 11 init

---
+++ Maso2187_l11/src/functions.py
@@ -0,0 +1,33 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Roan Somers Mason
+ID:      169072187
+Email:   maso2187@mylaurier.ca
+__updated__ = "2023-11-30"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def generate_matrix_num(rows, cols, low, high, value_type):
+    """
+    -------------------------------------------------------
+    Generates a 2D list of numbers of the given type, 'float' or 'int'.
+    (To generate random float number use random.uniform and to
+    generate random integer number use random.randint)
+    Use: matrix = generate_matrix_num(rows, cols, low, high, value_type)
+    -------------------------------------------------------
+    Parameters:
+        rows - number of rows in the list (int > 0)
+        cols - number of columns (int > 0)
+        low - low value of range (float)
+        high - high value of range (float > low)
+        value_type - type of values in the list, 'float' or 'int' (str)
+    Returns:
+        matrix - a 2D list of random numbers (2D list of float/int)
+    -------------------------------------------------------
+    """
